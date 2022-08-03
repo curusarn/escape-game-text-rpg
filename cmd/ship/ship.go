c59ffa21183f0a4e90cb86f89ef75c66f141d59e
Simon Let <simon.let@showmax.com>
2022-08-03 02:13:41 +0200

    add actual map

--- cmd/ship/ship.go
+++ cmd/ship/ship.go
@@ -11,6 +11,10 @@ func main() {
 		PeakMsg:     "the edge of the world",
 		CantMoveMsg: "You cannot sail to the edge of the world.",
 	}
+	l := terrain.Terrain{
+		PeakMsg:     "land",
+		CantMoveMsg: "You cannot onto land.",
+	}
 	var openSea string = "an open sea"
 	o := terrain.Terrain{
 		PeakMsg: openSea,
@@ -19,12 +23,28 @@ func main() {
 		PeakMsg:  openSea,
 		DeathMsg: "You have sailed onto a mine, your ship exploded into a thousand pieces and you died.",
 	}
+	f := terrain.Terrain{
+		PeakMsg:  openSea,
+		DeathMsg: "TODO: You have sailed into a harbour and you have won.",
+	}
 	arr := [][]terrain.Terrain{
-		{x, x, x, o, o, o},
-		{x, o, o, o, o, o},
-		{x, o, o, o, o, o},
-		{x, o, o, o, o, o},
-		{x, o, o, o, o, o},
+		{o, x, l, l, l, l, l, l, l, l, o, l, l, o, l, o, x, o, o, o, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, o, o},
+		{x, o, o, o, o, o, l, l, l, l, o, l, l, x, o, o, o, x, l, l, l, l, l, x, l, l, l, l, l, l, l, l, l, l, o, l, o, o, x, o},
+		{o, o, x, o, o, o, l, l, l, l, l, l, l, o, o, x, o, o, f, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, o, x, o, o, o, x},
+		{o, o, o, o, o, o, l, l, x, l, l, l, o, o, x, o, o, o, l, l, o, l, l, l, l, l, l, l, l, l, l, l, l, l, o, o, o, o, o, o},
+		{o, o, o, o, o, x, o, l, l, l, l, o, o, x, o, o, x, o, l, l, l, l, l, l, l, l, l, l, l, l, l, l, l, x, o, o, o, o, o, o},
+		{o, x, o, o, o, o, o, o, l, l, o, x, o, o, x, x, o, x, l, l, l, l, x, l, l, l, x, l, l, l, l, l, o, o, o, o, o, o, o, o},
+		{o, o, o, o, o, o, o, o, o, o, l, l, l, o, o, o, o, x, l, l, l, l, l, l, l, o, o, l, o, l, l, o, l, x, o, x, o, o, o, o},
+		{o, o, o, o, o, x, o, o, o, o, o, l, l, l, x, o, o, o, o, o, l, l, l, l, x, o, o, o, x, l, l, l, o, o, x, o, o, o, o, o},
+		{o, o, o, o, o, o, o, o, o, o, o, l, l, l, l, x, o, o, o, o, x, l, l, l, o, o, o, o, o, o, l, l, o, l, l, o, o, o, o, o},
+		{o, o, o, o, o, o, o, o, o, o, o, o, l, l, l, o, o, o, o, o, o, l, l, l, l, o, o, o, o, o, o, x, l, l, l, o, o, o, o, o},
+		{o, o, o, o, o, o, o, o, o, o, o, o, l, l, o, o, o, o, x, o, o, l, l, x, o, o, o, o, o, o, o, l, l, l, l, l, o, o, o, o},
+		{o, o, o, o, o, o, o, o, o, o, o, o, l, l, o, o, o, o, o, o, o, o, o, o, o, o, o, x, o, o, o, o, o, o, l, o, x, o, o, x},
+		{o, o, o, o, o, o, o, o, o, o, o, o, l, x, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o},
+		{o, o, o, o, o, o, o, o, o, o, o, l, l, o, o, o, o, x, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o},
+		{o, o, o, o, o, o, o, o, o, o, o, x, o, o, o, o, o, o, o, o, o, o, o, o, x, o, o, o, o, o, x, o, o, o, o, o, o, o, o, o},
+		{o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o},
+		{o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, x, o, o, o, o, o, o, o, o, x, o, o, o, o, o, o, o, o, o, o, o, o},
 	}
 	gameMap := game.BuildGameMapFromSlice(arr)
 	intro := `
@@ -41,7 +61,7 @@ Exit with 'exit'.
 	`
 	g := game.Game{
 		GameMap:        gameMap,
-		PlayerPosition: position.Position{X: 2, Y: 2},
+		PlayerPosition: position.Position{X: 21, Y: 14},
 		NothingTerrain: e,
 		MoveMsg:        "You have sailed ",
 		Intro:          intro,
